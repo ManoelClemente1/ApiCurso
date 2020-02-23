7adccef85224096271f17447c7fde21d764316b3
Manoel C <manoelclemente9@outlook.com>
2020-02-23 18:36:16 -0300

    Order, instant, iso 8601 parte 2

---
+++ curso/src/main/java/com/workshop/curso/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.workshop.curso.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.workshop.curso.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+
+}
